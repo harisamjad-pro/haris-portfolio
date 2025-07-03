559ff54677eed78c2efe14529c4f96bac4b12c8a
harisamjad-pro <harisamjad56@gmail.com>
2025-07-03 06:19:41 +0500

    Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
   return (
     <header className="fixed top-0 w-full">
       <nav className="px-24 max-xl:px-16 max-lg:px-12 max-md:px-8 max-sm:px-4 py-6 flex items-center justify-center">
-        <ul className="px-2 py-2 flex items-center gap-6 bg-neutral-200/40 backdrop-blur-xl rounded-full text-sm font-medium text-black">
-          <li>
+        <ul className="px-2 py-2 bg-neutral-200/40 backdrop-blur-xl rounded-full text-sm font-medium text-black max-sm:w-full">
+          <li className="flex items-center justify-between gap-16 max-sm:gap-6">
             <Link href="/" className="relative">
               <Image
                 src="/Haris.jpg"
@@ -18,8 +18,19 @@ export default function Header() {
               />
               <div className="w-full h-full bg-green-400 rounded-full animate-ping absolute left-0 top-0"></div>
             </Link>
+            <ul className="flex items-center gap-6">
+              <li>
+                <Link href="/">About</Link>
+              </li>
+              <li>
+                <Link href="/">Work</Link>
+              </li>
+              <li>
+                <Link href="/" className="bg-neutral-800 text-green-400 px-4 py-2 rounded-full">Contact</Link>
+              </li>
+            </ul>
           </li>
-          <li>
+          {/* <li>
             <Link href="/">About</Link>
           </li>
           <li>
@@ -27,7 +38,7 @@ export default function Header() {
           </li>
           <li>
             <Link href="/" className="bg-neutral-800 text-green-400 px-4 py-2 rounded-full">Contact</Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
