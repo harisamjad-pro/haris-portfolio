fa70e7b1d1fc6a8369120200fab1b97a5a3b527f
harisamjad-pro <harisamjad56@gmail.com>
2025-07-03 06:22:40 +0500

    Header V2

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
   return (
     <header className="fixed top-0 w-full">
       <nav className="px-24 max-xl:px-16 max-lg:px-12 max-md:px-8 max-sm:px-4 py-6 flex items-center justify-center">
-        <ul className="px-2 py-2 bg-neutral-200/40 backdrop-blur-xl rounded-full text-sm font-medium text-black max-sm:w-full">
-          <li className="flex items-center justify-between gap-16 max-sm:gap-6">
+        <ul className="px-2 py-2 bg-neutral-200/40 backdrop-blur-xl rounded-full text-base font-medium text-black max-sm:w-full">
+          <li className="flex items-center justify-between gap-24 max-sm:gap-6">
             <Link href="/" className="relative">
               <Image
                 src="/Haris.jpg"
@@ -16,7 +16,7 @@ export default function Header() {
                 height={96}
                 className="w-12 h-12 object-cover rounded-full relative z-20"
               />
-              <div className="w-full h-full bg-green-400 rounded-full animate-ping absolute left-0 top-0"></div>
+              <div className="w-full h-full bg-green-400 rounded-full animate-ping absolute z-10 left-0 top-0"></div>
             </Link>
             <ul className="flex items-center gap-6">
               <li>
