b39bc0a066ebf60410172ec9ad966d8f54f2e9df
harisamjad-pro <harisamjad56@gmail.com>
2025-07-03 06:07:21 +0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <h1>Hi, I'm Haris. A Full Stack Developer with love for clean UI and fast apps</h1>
+    <div className="mt-44">
+      <div className="px-24 max-xl:px-16 max-lg:px-12 max-md:px-8 max-sm:px-4 grid gap-24 w-full justify-center">
+        <div className="w-2xl max-sm:w-full">
+          <h1 className="text-5xl max-md:text-4xl leading-14 max-md:leading-11 text-center font-semibold text-neutral-500 flex flex-col"><span className="text-black">Hi, I'm Haris.</span>Full Stack Developer with love for clean UI and fast apps.</h1>
+        </div>
+        <div className="w-2xl max-sm:w-full">
+          <h1 className="text-5xl max-md:text-4xl leading-14 max-md:leading-11 text-center font-semibold text-neutral-500 flex flex-col"><span className="text-black">Hi, I'm Haris.</span>Full Stack Developer with love for clean UI and fast apps.</h1>
+        </div>
+        <div className="w-2xl max-sm:w-full">
+          <h1 className="text-5xl max-md:text-4xl leading-14 max-md:leading-11 text-center font-semibold text-neutral-500 flex flex-col"><span className="text-black">Hi, I'm Haris.</span>Full Stack Developer with love for clean UI and fast apps.</h1>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 w-full">
+      <nav className="px-24 max-xl:px-16 max-lg:px-12 max-md:px-8 max-sm:px-4 py-6 flex items-center justify-center">
+        <ul className="px-2 py-2 flex items-center gap-6 bg-neutral-200/40 backdrop-blur-xl rounded-full text-sm font-medium text-black">
+          <li>
+            <Link href="/" className="relative">
+              <Image
+                src="/Haris.jpg"
+                alt="Haris Amjad"
+                width={96}
+                height={96}
+                className="w-12 h-12 object-cover rounded-full relative z-20"
+              />
+              <div className="w-10 h-10 bg-green-400 rounded-full animate-ping absolute left-1 top-1"></div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">About</Link>
+          </li>
+          <li>
+            <Link href="/">Work</Link>
+          </li>
+          <li>
+            <Link href="/" className="bg-neutral-800 text-green-400 px-4 py-2 rounded-full">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
